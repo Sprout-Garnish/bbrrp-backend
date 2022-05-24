8a2a22c292ee86957cf5c90c1e3f20754f10a487
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-05-24 13:13:43 +0900

    add cors policy

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ import { PORT } from "./config";
 export const server: ServerConfig<BaseKeystoneTypeInfo> = {
   port: PORT,
   healthCheck: true,
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
+  },
 };
